b3b9f1ace20bcd636cc88e434755e75820b4349e
sveta290700 <777rsb@mail.ru>
2021-12-01 19:00:12 +1000

    [14] PHPCBF

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -1,18 +1,15 @@
 <?php
 
 namespace App\Controller;
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
 class TestController extends AbstractController
 {
-    public function index()
+    public function index():Response
     {
         return $this->json([
             'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/TestController.php',
-        ]);
+            'path' => 'src/Controller/TestController.php',]);
     }
 }
